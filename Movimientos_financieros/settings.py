599536b698325a8ef0e6a234829e36af10cbcdab
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2025-07-04 16:58:40 -0500

    Actualiza la configuración de seguridad en settings.py: se elimina la clave secreta por defecto y se permite el acceso desde cualquier host.

--- Movimientos_financieros/settings.py
+++ Movimientos_financieros/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-(jce(*kkg4gi1c#xq*dnz^_#t(=)$h1&l!2!=4bfa9y*_=7*4!")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
